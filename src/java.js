2c7efdca471302df8e56b2b2ccaae172651ea1c4
JoaoAHaupt <joao.haupt@aluno.ifsp.edu.br>
2023-09-26 23:28:26 -0300

    cores implementadas

--- src/java.js
+++ src/java.js
@@ -1,7 +1,7 @@
 const apiKey="c0f4b40f836f69420f6281c4a0b9ca24";
 const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const caixa = document.querySelector("#caixa");
+const caixa = document.querySelector(".caixa"); 
 const pesquisa = document.querySelector(".pesquisa input");
 const pesquisaBotao = document.querySelector(".pesquisa button");
 const iconeClima= document.querySelector(".icone-clima")
@@ -20,21 +20,30 @@ async function checkWeather(cidade){
 
     if(data.weather[0].main == "Clouds"){
         iconeClima.src = "imagens/nuvens.png";
-        caixa.style.background = "linear-gradient(70deg, #0deb5b, #e2463e)";
+        caixa.style.setProperty("background", "linear-gradient(70deg, #999c64, #eddb11)", "important");
+        caixa.style.setProperty("box-shadow", "0px 10px 30px #999c64", "important");
     }
+
     else if(data.weather[0].main == "Clear"){
         iconeClima.src = "imagens/sol.png";
-        caixa.style.backgroundColor = "linear-gradient(70deg, #0deb5b, #e2463e);";
+        caixa.style.setProperty("background", "linear-gradient(70deg, #e6960e, #d62222)", "important");
+        caixa.style.setProperty("box-shadow", "0px 10px 30px #e6960e", "important");
 
     }
     else if(data.weather[0].main == "Rain"){
         iconeClima.src = "imagens/chuva.png";
+        caixa.style.setProperty("background", "linear-gradient(70deg, #38aae8, #1c1fe6)", "important");
+        caixa.style.setProperty("box-shadow", "0px 10px 30px #38aae8", "important");
     }
     else if(data.weather[0].main == "Drizzle"){
         iconeClima.src = "imagens/garoa.png";
+        caixa.style.setProperty("background", "linear-gradient(70deg, #e8d638, #1c1fe6)", "important");
+        caixa.style.setProperty("box-shadow", "0px 10px 30px #e8d638", "important");
     }
     else if(data.weather[0].main == "Mist"){
         iconeClima.src = "imagens/nublado.png";
+        caixa.style.setProperty("background", "linear-gradient(70deg, #1932bf, #9ca5db)", "important");
+        caixa.style.setProperty("box-shadow", "0px 10px 30px #1932bf", "important");
     }
 
 }
